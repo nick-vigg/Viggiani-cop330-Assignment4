b3828e79f8e004c6fd0466d61a1638a155e5f7ca
nick-vigg <nviggiani@knights.ucf.edu>
2021-07-05 20:46:38 -0400

    Starting Commit

---
+++ src/main/java/ucf/assignments/ItemController.java
@@ -0,0 +1,23 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+
+public class ItemController {
+    public void addItemButtonClicked(ActionEvent actionEvent) {
+        //The user will input the necessary data
+        //After the data is set, the user will press the button
+        //And the item will be added to the current list
+    }
+
+    public void itemDueDateSet(ActionEvent actionEvent) {
+        //User will select a Due Date for the item
+    }
+
+    public void itemDescriptionSet(ActionEvent actionEvent) {
+        //User will write a description of the item
+    }
+
+    public void isCompleteSet(ActionEvent actionEvent) {
+        //User will describe whether the item has been complete or not
+    }
+}
---
+++ src/main/java/ucf/assignments/ListController.java
@@ -0,0 +1,20 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+
+public class ListController {
+    public void addListButtonClick(ActionEvent actionEvent) {
+        //User will input a title of a new to-do-list
+        //to-do-list will be added to list of to-do-list lists
+    }
+
+    public void deleteListButtonClick(ActionEvent actionEvent) {
+        //User will click name of to-do-list to be deleted
+        //User will click delete button to remove to-do-list
+    }
+
+    public void displayListButtonClick(ActionEvent actionEvent) {
+        //User will click on a title of a to-do-list
+        //User will then click Show Items button to display Items in to-do-list
+    }
+}
