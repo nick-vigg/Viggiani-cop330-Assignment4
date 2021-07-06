161eaac5c39b47b5cba5baf59019906cf8a1c3f9
nick-vigg <nviggiani@knights.ucf.edu>
2021-07-05 22:48:25 -0400

    Committed everything but file dealings

--- src/main/java/ucf/assignments/App.java
+++ src/main/java/ucf/assignments/App.java
@@ -6,6 +6,7 @@ package ucf.assignments;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -14,16 +15,23 @@ import java.io.IOException;
 
 import static javafx.application.Application.launch;
 
-public class App {
-    //@Override
+public class App extends Application{
+    @Override
     public void start(Stage stage) {
         Parent root1 = null;
+        Parent root2 = null;
         try {
             root1 = FXMLLoader.load(getClass().getResource("ass4.fxml"));
             Scene scene1 = new Scene(root1);
             stage.setTitle("To-Do-Lists");
             stage.setScene(scene1);
             stage.show();
+           /* root2 = FXMLLoader.load(getClass().getResource("item.fxml"));
+            Scene scene2 = new Scene(root2);
+            stage.setTitle("Items");
+            stage.setScene(scene2);
+            stage.show(); */
+
         } catch (IOException e) {
             e.printStackTrace();
         }
